f790cbf7403e5ba8c2a1b82aefbe10e55f7292f5
surenderyadav29 <surenderpota314@gmail.com>
2026-07-06 11:09:51 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('a.nav-link').forEach(link => {
-link.addEventListener('click', function(e) {
-e.preventDefault();
-document.querySelector(this.getAttribute('href'))
-.scrollIntoView({ behavior: 'smooth' });
-});
-});
